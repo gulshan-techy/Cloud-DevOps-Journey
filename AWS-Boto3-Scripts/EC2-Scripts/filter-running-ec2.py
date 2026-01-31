326d87abbdb08c260754c9d2dbbd4f2553723bc6
Gulshan Kumar <gulsanshakya123@gmail.com>
2026-01-31 15:30:22 +0530

    Add script to filter running and stopped EC2 instances
    This script filters and prints details of both running and stopped EC2 instances in the specified region.

---
+++ AWS-Boto3-Scripts/EC2-Scripts/filter-running-ec2.py
@@ -0,0 +1,22 @@
+# FILTER RUNNING/STOP/STOPPING/TERMINATED EC2s 
+
+
+# This Code Filter "RUNNING" EC2s Instances and Print Instance ID , State , Type
+import boto3
+ec2=boto3.client("ec2", region_name="eu-north-1")
+response=ec2.describe_instances(
+    Filters=[{"Name":"instance-state-name","Values":["running"]}])
+
+for res in response["Reservations"]:
+    for ins in res["Instances"]:
+        print(f"ID: {ins['InstanceId']} | State: {ins['State']['Name']} | {ins['InstanceType']}") 
+
+# This Code Filter "STOPPED" EC2s Instances Instances and Print Instance ID , State , Type
+import boto3
+ec2=boto3.client("ec2", region_name="eu-north-1")
+response=ec2.describe_instances(
+    Filters=[{"Name":"instance-state-name","Values":["stopped"]}])
+
+for res in response["Reservations"]:
+    for ins in res["Instances"]:
+        print(f"ID: {ins['InstanceId']} | State: {ins['State']['Name']} | {ins['InstanceType']}")
